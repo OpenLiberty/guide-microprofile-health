31f62bd28114c87b4bef497a5030efe09f45b7ee
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-01-19 17:34:34 -0500

    Restructured MP-Health

---
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -0,0 +1,65 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.inventory;
+
+// CDI
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+// JSON-P
+import javax.json.JsonObject;
+
+// JAX-RS
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+// tag::JsonMessages[]
+import io.openliberty.guides.common.JsonMessages;
+// end::JsonMessages[]
+import io.openliberty.guides.inventory.InventoryConfig;
+
+@RequestScoped
+@Path("hosts")
+public class InventoryResource {
+  @Inject
+  InventoryManager manager;
+
+  // tag::config[]
+  @Inject
+  InventoryConfig config;
+  // end::config[]
+
+  // tag::getPropertiesForHost[]
+  @GET
+  @Path("{hostname}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public JsonObject getPropertiesForHost(
+      @PathParam("hostname") String hostname) {
+    return config.isInMaintenance()
+        ? JsonMessages.serviceInMaintenance(InventoryResource.class.getSimpleName())
+        : manager.get(hostname, config.getPortNumber());
+  }
+  // end::getPropertiesForHost[]
+
+  // tag::listContents[]
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public JsonObject listContents() {
+    return config.isInMaintenance()
+        ? JsonMessages.serviceInMaintenance(InventoryResource.class.getSimpleName())
+        : manager.list();
+  }
+  // end::listContents[]
+}
---
+++ finish/src/test/java/it/io/openliberty/guides/health/HealthTest.java
@@ -0,0 +1,80 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::comment[]
+package it.io.openliberty.guides.health;
+
+import static org.junit.Assert.*;
+import java.util.HashMap;
+import javax.json.JsonArray;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HealthTest {
+  // tag::data[]
+  private JsonArray servicesStates;
+  private HashMap<String, String> dataWhenServicesUP, dataWhenInventoryDown;
+  public static final String INV_MAINTENANCE = "io_openliberty_guides_inventory_inMaintenance";
+
+  @Before
+  public void setup() {
+    dataWhenServicesUP = new HashMap<String, String>();
+    dataWhenInventoryDown = new HashMap<String, String>();
+
+    dataWhenServicesUP.put("PropertiesResource", "UP");
+    dataWhenServicesUP.put("InventoryResource", "UP");
+
+    dataWhenInventoryDown.put("PropertiesResource", "UP");
+    dataWhenInventoryDown.put("InventoryResource", "DOWN");
+  }
+  // end::data[]
+
+  // tag::testIfServicesStatesUp[]
+  @Test
+  public void testIfServicesStatesUp() {
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(200);
+    checkServicesStates(dataWhenServicesUP, servicesStates);
+  }
+  // end::testIfServicesStatesUp[]
+
+  // tag::testIfInventoryTemporarilyDown[]
+  @Test
+  public void testIfInventoryTemporarilyDown() {
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(200);
+    checkServicesStates(dataWhenServicesUP, servicesStates);
+    HealthTestUtil.changeInventoryProperty(INV_MAINTENANCE + "\": false",
+                                           INV_MAINTENANCE + "\": true");
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(503);
+    checkServicesStates(dataWhenInventoryDown, servicesStates);
+  }
+  // end::testIfInventoryTemporarilyDown[]
+
+  // tag::checkServicesStates[]
+  private void checkServicesStates(HashMap<String, String> testData,
+      JsonArray servicesStates) {
+    testData.forEach((service, expectedState) -> {
+      assertEquals("The state of " + service + " service is not matching the ",
+                   expectedState,
+                   HealthTestUtil.getActualState(service, servicesStates));
+    });
+
+  }
+  // end::checkServicesStates[]
+
+  // tag::After[]
+  @After
+  public void teardown() {
+    HealthTestUtil.cleanUp();
+  }
+  // end::After[]
+
+}
