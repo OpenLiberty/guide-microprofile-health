3c8eec1c05f3276f7857a5cd6b826e824354d7f8
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-01-19 17:34:34 -0500

    Restructured MP-Health

--- start/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
+++ start/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
@@ -1,4 +1,4 @@
-// tag::comment[]
+// tag::copyright[]
 /*******************************************************************************
  * Copyright (c) 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
@@ -9,100 +9,102 @@
  * Contributors:
  *     IBM Corporation - Initial implementation
  *******************************************************************************/
-// end::comment[]
+// end::copyright[]
+
+// tag::customConfig[]
 package io.openliberty.guides.config;
 
 import javax.json.stream.JsonParser;
 import javax.json.stream.JsonParser.Event;
-
 import javax.json.Json;
 import java.math.BigDecimal;
-import org.eclipse.microprofile.config.spi.ConfigSource;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.util.*;
 import java.io.StringReader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
 public class CustomConfigSource implements ConfigSource {
-    String fileLocation = System.getProperty("user.dir").split("target")[0] + "CustomConfigSource.json";
-    Map<String, String> map = setProperties();
 
-    @Override
-    public int getOrdinal() {
-        return Integer.parseInt(this.map.get("config_ordinal"));
-    }
+  String fileLocation = System.getProperty("user.dir").split("target")[0]
+      + "resource/CustomConfigSource.json";
+  Map<String, String> map = setProperties();
 
-    @Override
-    public Set<String> getPropertyNames() {
-        return this.map.keySet();
-    }
+  @Override
+  public int getOrdinal() {
+    return Integer.parseInt(this.map.get("config_ordinal"));
+  }
 
-    @Override
-    public Map<String, String> getProperties() {
-        return this.map;
-    }
+  @Override
+  public Set<String> getPropertyNames() {
+    return this.map.keySet();
+  }
 
-    @Override
-    public String getValue(String key) {
-        return this.map.get(key);
-    }
+  @Override
+  public Map<String, String> getProperties() {
+    return this.map;
+  }
 
-    @Override
-    public String getName() {
-        return "Custom Config Source: file:" + this.fileLocation;
-    }
+  @Override
+  public String getValue(String key) {
+    return this.map.get(key);
+  }
 
-    private Map<String, String> setProperties() {
-        System.out.println(fileLocation);
-        Map<String, String> m = new HashMap<String, String>();
-        String jsonData = readFile(this.fileLocation);
-        JsonParser parser = Json.createParser(new StringReader(jsonData));
-        String key = null;
-        while (parser.hasNext()) {
-            final Event event = parser.next();
-            switch (event) {
-            case KEY_NAME:
-                key = parser.getString();
-                break;
-            case VALUE_STRING:
-                String string = parser.getString();
-                m.put(key, string);
-                break;
-            case VALUE_NUMBER:
-                BigDecimal number = parser.getBigDecimal();
-                m.put(key, number.toString());
-                break;
-            case VALUE_TRUE:
-                m.put(key, "true");
-                break;
-            case VALUE_FALSE:
-                m.put(key, "false");
-                break;
-            }
-        }
-        parser.close();
-        return m;
-    }
+  @Override
+  public String getName() {
+    return "Custom Config Source: file:" + this.fileLocation;
+  }
 
-    private static String readFile(String filename) {
-        String result = "";
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line);
-                line = br.readLine();
-            }
-            result = sb.toString();
-            br.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+  private Map<String, String> setProperties() {
+    Map<String, String> m = new HashMap<String, String>();
+    String jsonData = this.readFile(this.fileLocation);
+    JsonParser parser = Json.createParser(new StringReader(jsonData));
+    String key = null;
+    while (parser.hasNext()) {
+      final Event event = parser.next();
+      switch (event) {
+      case KEY_NAME:
+        key = parser.getString();
+        break;
+      case VALUE_STRING:
+        String string = parser.getString();
+        m.put(key, string);
+        break;
+      case VALUE_NUMBER:
+        BigDecimal number = parser.getBigDecimal();
+        m.put(key, number.toString());
+        break;
+      case VALUE_TRUE:
+        m.put(key, "true");
+        break;
+      case VALUE_FALSE:
+        m.put(key, "false");
+        break;
+      default:
+        break;
+      }
     }
+    parser.close();
+    return m;
+  }
+
+  public String readFile(String fileName) {
+    String result = "";
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(fileName));
+      StringBuilder sb = new StringBuilder();
+      String line = br.readLine();
+      while (line != null) {
+        sb.append(line);
+        line = br.readLine();
+      }
+      result = sb.toString();
+      br.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return result;
+  }
 }
+// end::customConfig[]
