d5c3c577af6bbfb882f225099927307292f716e2
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-01-30 17:25:39 -0500

    updated adoc

--- finish/src/test/java/it/io/openliberty/guides/inventory/EndpointTest.java
+++ finish/src/test/java/it/io/openliberty/guides/inventory/EndpointTest.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,8 @@
 // tag::testClass[]
 package it.io.openliberty.guides.inventory;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
