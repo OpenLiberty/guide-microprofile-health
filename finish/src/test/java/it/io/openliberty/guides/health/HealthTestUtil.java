7f98157a5ef67d97c7eae18347806a99c3c29c10
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-01-30 17:25:39 -0500

    updated adoc

--- finish/src/test/java/it/io/openliberty/guides/health/HealthTestUtil.java
+++ finish/src/test/java/it/io/openliberty/guides/health/HealthTestUtil.java
@@ -1,6 +1,6 @@
 // tag::comment[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,7 @@
 // tag::HealthTestUtil[]
 package it.io.openliberty.guides.health;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,12 +31,21 @@ import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
 
 public class HealthTestUtil {
 
+  private static String port;
+  private static String baseUrl;
+  private final static String HEALTH_ENDPOINT = "health";
+
+  static {
+    port = System.getProperty("liberty.test.port");
+    baseUrl = "http://localhost:" + port + "/";
+  }
+
   public static JsonArray connectToHealthEnpoint(int expectedResponseCode) {
-    String url = "http://localhost:9080/health";
+    String healthURL = baseUrl + HEALTH_ENDPOINT;
     Client client = ClientBuilder.newClient().register(JsrJsonpProvider.class);
-    Response response = client.target(url).request().get();
-    assertEquals("Response code is not matching " + url, expectedResponseCode,
-                 response.getStatus());
+    Response response = client.target(healthURL).request().get();
+    assertEquals("Response code is not matching " + healthURL,
+                 expectedResponseCode, response.getStatus());
     JsonArray servicesStates = response.readEntity(JsonObject.class)
                                        .getJsonArray("checks");
     response.close();
@@ -60,7 +69,7 @@ public class HealthTestUtil {
   public static void changeInventoryProperty(String oldValue, String newValue) {
     try {
       String fileName = System.getProperty("user.dir").split("target")[0]
-          + "/resource/CustomConfigSource.json";
+          + "/../resources/CustomConfigSource.json";
       BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
       String line = "";
       String oldContent = "", newContent = "";
@@ -78,8 +87,8 @@ public class HealthTestUtil {
   }
 
   public static void cleanUp() {
-    changeInventoryProperty(HealthTest.INV_MAINTENANCE + "\": true",
-                            HealthTest.INV_MAINTENANCE + "\": false");
+    changeInventoryProperty(HealthTest.INV_MAINTENANCE_TRUE,
+                            HealthTest.INV_MAINTENANCE_FALSE);
   }
 
 }
