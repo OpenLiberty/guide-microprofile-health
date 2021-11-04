2ee761f0fb576c15146ce6fe5866f7ab571e40a2
Kevin Cho <Kevcpro@users.noreply.github.com>
2021-11-04 10:32:39 -0400

    Update HealthIT.java

--- finish/src/test/java/it/io/openliberty/guides/health/HealthIT.java
+++ finish/src/test/java/it/io/openliberty/guides/health/HealthIT.java
@@ -31,6 +31,7 @@ public class HealthIT {
   private String HEALTH_ENDPOINT = "health";
   private String READINESS_ENDPOINT = "health/ready";
   private String LIVENES_ENDPOINT = "health/live";
+  private String STARTUP_ENDPOINT = "health/started";
 
   @BeforeEach
   public void setup() {
@@ -73,6 +74,18 @@ public class HealthIT {
     checkStates(endpointData, servicesStates);
   }
   // end::testLiveness[]
+
+  @Test
+  // tag::testStartup[]
+  public void testStartup() {
+    endpointData.put("SystemResource Startup Check", "UP");
+    endpointData.put("InventoryResource Startup Check", "UP");
+
+    servicesStates = HealthITUtil.connectToHealthEnpoint(200, STARTUP_ENDPOINT);
+    checkStates(endpointData, servicesStates);
+  }
+  // end::testStartup[]
+
 
   @Test
   // tag::testIfInventoryServiceIsDown[]
