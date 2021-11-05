88e80863ba950f7c6f6aa225c52649cd0d8b911f
Kevin Cho <Kevcpro@users.noreply.github.com>
2021-11-05 15:50:47 -0400

    Update InventoryStartupCheck.java

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryStartupCheck.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryStartupCheck.java
@@ -34,9 +34,9 @@ public class InventoryStartupCheck implements HealthCheck {
         OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)
         ManagementFactory.getOperatingSystemMXBean();
         if (bean.getSystemCpuLoad() < 0.95) {
-           return HealthCheckResponse.up(STARTUP_CHECK + bean.getSystemCpuLoad());
+           return HealthCheckResponse.up(String.valueOf(bean.getSystemCpuLoad() < 0.95));
         } else {
-           return HealthCheckResponse.down(STARTUP_CHECK + bean.getSystemCpuLoad());
+           return HealthCheckResponse.down(String.valueOf(bean.getSystemCpuLoad() < 0.95));
         }
     }
 }
