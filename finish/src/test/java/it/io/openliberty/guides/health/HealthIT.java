1dcfc403b6f80506579b06459512a2f1a7a98a77
Ryan Storey <ryanstorey999@gmail.com>
2022-01-07 14:52:18 +0000

    Add testStartup() tests and enchance testIfServicesAreUp() and testIfInventoryServiceIsDown()

--- finish/src/test/java/it/io/openliberty/guides/health/HealthIT.java
+++ finish/src/test/java/it/io/openliberty/guides/health/HealthIT.java
@@ -15,6 +15,7 @@ package it.io.openliberty.guides.health;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
 import java.util.HashMap;
 
 import javax.json.JsonArray;
@@ -31,6 +32,7 @@ public class HealthIT {
   private String HEALTH_ENDPOINT = "health";
   private String READINESS_ENDPOINT = "health/ready";
   private String LIVENES_ENDPOINT = "health/live";
+  private String STARTUP_ENDPOINT = "health/started";
 
   @BeforeEach
   public void setup() {
@@ -44,11 +46,24 @@ public class HealthIT {
     endpointData.put("SystemResource Liveness Check", "UP");
     endpointData.put("InventoryResource Readiness Check", "UP");
     endpointData.put("InventoryResource Liveness Check", "UP");
+    endpointData.put("InventoryResource Startup Check", "UP");
+    endpointData.put("SystemResource Startup Check", "UP");
 
     servicesStates = HealthITUtil.connectToHealthEnpoint(200, HEALTH_ENDPOINT);
     checkStates(endpointData, servicesStates);
   }
   // end::testIfServicesAreUp[]
+
+  @Test
+  // tag::testStartup[]
+  public void testStartup() {
+    endpointData.put("InventoryResource Startup Check", "UP");
+    endpointData.put("SystemResource Startup Check", "UP");
+
+    servicesStates = HealthITUtil.connectToHealthEnpoint(200, STARTUP_ENDPOINT);
+    checkStates(endpointData, servicesStates);
+  }
+  // end::testStartup[]
 
   @Test
   // tag::testReadiness[]
@@ -79,6 +94,8 @@ public class HealthIT {
     endpointData.put("SystemResource Liveness Check", "UP");
     endpointData.put("InventoryResource Readiness Check", "UP");
     endpointData.put("InventoryResource Liveness Check", "UP");
+    endpointData.put("InventoryResource Startup Check", "UP");
+    endpointData.put("SystemResource Startup Check", "UP");
 
     servicesStates = HealthITUtil.connectToHealthEnpoint(200, HEALTH_ENDPOINT);
     checkStates(endpointData, servicesStates);
